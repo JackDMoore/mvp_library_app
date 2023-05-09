1582aea9895066c196ec3d09595f95f0d1d960b7
angela2264 <angela2264@hotmail.com>
2023-05-09 20:53:32 +0100

    attempt to fix update

--- api/models/post.js
+++ api/models/post.js
@@ -1,12 +1,11 @@
 const db = require('../database/connect');
 
 class Post {
-
-    constructor({ post_id, title, content, book_year, author, genre, on_loan }) {
+    constructor({post_id, title, content, book_year, author, genre, on_loan,}) {
         this.id = post_id;
         this.title = title;
         this.content = content;
-        this.book_year = book_year
+        this.year = book_year;
         this.author = author;
         this.genre = genre;
         this.on_loan = on_loan;
@@ -26,8 +25,8 @@ class Post {
     }
 
     static async create(data) {
-        const { title, content, book_year, author, genre, on_loan } = data;
-        let response = await db.query("INSERT INTO post (title, content, book_year, author, genre, on_loan) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;",
+        const { title, content, book_year, author, genre, on_loan=false} = data;
+        let response = await db.query("INSERT INTO post (title, content,book_year,author,genre,on_loan) VALUES ($1, $2,$3,$4,$5,$6) RETURNING post_id;",
             [title, content, book_year, author, genre, on_loan]);
         const newId = response.rows[0].post_id;
         const newPost = await Post.getOneById(newId);
@@ -42,14 +41,15 @@ class Post {
                 throw new Error("Not able to update book")
               }
               return new Post(response.rows[0])
-            }
+    }
     
-
     async destroy() {
         let response = await db.query("DELETE FROM post WHERE post_id = $1 RETURNING *;", [this.id]);
-        return new Post(response.rows[0]);
+            if (response.rows.length != 1) {
+                    throw new Error("Unable to delete book.")
+                }
+                return new Post(response.rows[0]);
     }
-
 }
 
 module.exports = Post;
