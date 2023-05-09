b31a3a49c6a8d38eb7dc30db63c577354f329806
Sean <sean_obeirne@outlook.com>
2023-05-09 13:21:41 +0100

    First test of API passed

--- api/__tests__/api.spec.js
+++ api/__tests__/api.spec.js
@@ -5,13 +5,17 @@ describe("api server", () => {
   let api;
   //before all is a jest hook, that runs before(or after) all the tests, but will only run once
   beforeAll(() => {
-    api = app.listen(3000, () => {
-      console.log("Test server running on port 3000");
+    api = app.listen(5000, () => {
+      console.log("Test server running on port 5000");
     });
   });
 
   afterAll((done) => {
     console.log('Stopping the test server')
     api.close(done)
+  })
+
+  test('it responds to get / with status 200', () => {
+
   })
 });
